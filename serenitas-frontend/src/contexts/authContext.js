44e5cdaea489ce36e0f878523dff400b95f8e129
Edoardo Giambuzzi <edoardo.giambuzzi@gmail.com>
2024-03-09 09:40:23 +0100

    separate backend & frontend

--- serenitas-frontend/src/contexts/authContext.js
+++ serenitas-frontend/src/contexts/authContext.js
@@ -5,6 +5,8 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null); // Initialize user state with null
   const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
@@ -16,6 +18,10 @@ export const AuthProvider = ({ children }) => {
         setIsLoggedIn,
         username,
         setUsername,
+        password,
+        setPassword,
+        email,
+        setEmail,
       }}
     >
       {children}
