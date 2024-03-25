0527b6b2c2f60397eed69e35e80783b91d2c3868
Edoardo Giambuzzi <edoardo.giambuzzi@gmail.com>
2024-03-25 10:59:34 +0100

    tent5

--- serenitas-backend/models/User.js
+++ serenitas-backend/models/User.js
@@ -5,12 +5,19 @@ const projectSchema = new mongoose.Schema({
   status: { type: String, required: true },
 });
 
+// Define indexes on projectSchema
+projectSchema.index({ name: 1 }); // Index on the 'name' field
+
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
   email: { type: String, required: true },
   projects: [projectSchema],
 });
+
+// Define indexes on userSchema
+userSchema.index({ username: 1 }, { unique: true }); // Index on 'username' field with uniqueness constraint
+userSchema.index({ email: 1 }, { unique: true }); // Index on 'email' field with uniqueness constraint
 
 const User = mongoose.model("User", userSchema);
 
