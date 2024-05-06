c3843e7e9e3e5f21e59c5564140015940e3ea499
Edoardo Giambuzzi <edoardo.giambuzzi@gmail.com>
2024-05-06 16:09:39 +0200

    new

--- serenitas-frontend/src/components/ContactForm.jsx
+++ serenitas-frontend/src/components/ContactForm.jsx
@@ -12,7 +12,13 @@ export const ContactForm = () => {
   return (
     <>
       <div className="mail-button-container">
-        <button onClick={handleButtonClick} className="mail-button"></button>
+        <button onClick={handleButtonClick} id="mail-button">
+          <img
+            className="mail-button-image"
+            src="https://res.cloudinary.com/dgwvbd9ki/image/upload/v1712126279/serenitas/djambo1990_51954_an_envelope_the_way_Jacques_Louis_David_would__de60a7e5-1a9f-4a5a-8118-7b651d54e0b7_bx6xzl-removebg-preview_fo0iik.png"
+            alt="contact-form"
+          />
+        </button>
       </div>
     </>
   );
